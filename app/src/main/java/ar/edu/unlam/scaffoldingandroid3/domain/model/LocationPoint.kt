c89d1e310ed43b82b7b27a529f24b96490b599c2
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2025-06-13 00:52:53 -0300

    Implementacion de modelo History y Photo

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/LocationPoint.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/LocationPoint.kt
@@ -1,10 +1,11 @@
 package ar.edu.unlam.scaffoldingandroid3.domain.model
 
 /**
- * TODO: Entidad de dominio - Punto GPS individual
+ * Entidad de dominio - Punto GPS individual
  * Representa coordenadas GPS con timestamp y datos de precisión/velocidad
  * LocationPoint tendría propiedades extra para tracking GPS
  */
+
 data class LocationPoint(
     // Precisión GPS (importante para filtering)
     val accuracy: Float,
