ec3cf1c8d233114d1abde4e9ce7e9d329e9c6c4f
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2025-06-13 20:08:13 -0300

    Implementacion de MapType y MapSettings

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/TrackingStatus.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/TrackingStatus.kt
@@ -1,7 +1,7 @@
 package ar.edu.unlam.scaffoldingandroid3.domain.model
 
 /**
- * num - Estados posibles de una sesión de tracking
+ * Enum - Estados posibles de una sesión de tracking
  * Valores: ACTIVE, PAUSED, COMPLETED para controlar flujo de tracking
  */
 
