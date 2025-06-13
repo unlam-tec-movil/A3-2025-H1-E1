2b72beefafb9505bc4405a0db9c0160462ee9ad8
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2025-06-13 01:21:46 -0300

    implementacion de TrackingSession y TrackingStatus

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/TrackingStatus.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/domain/model/TrackingStatus.kt
@@ -1,8 +1,12 @@
 package ar.edu.unlam.scaffoldingandroid3.domain.model
 
 /**
- * TODO: Enum - Estados posibles de una sesión de tracking
+ * num - Estados posibles de una sesión de tracking
  * Valores: ACTIVE, PAUSED, COMPLETED para controlar flujo de tracking
  */
 
-enum class TrackingStatus
+enum class TrackingStatus{
+    ACTIVE,
+    PAUSED,
+    COMPLETED,
+}
