24512871243d5cd80566d6458c1a8a19c1ce7c70
esmepantano <pantanoesmeralda@gmail.com>
2025-06-22 18:16:18 -0300

    HistoryUiState

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/history/HistoryUiState.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/history/HistoryUiState.kt
@@ -1,7 +1,14 @@
 package ar.edu.unlam.scaffoldingandroid3.ui.history
 
+import ar.edu.unlam.scaffoldingandroid3.domain.model.History
+
 /**
- * TODO: Data class - Estado UI del historial
+ * Data class - Estado UI del historial
  */
 
-data class HistoryUiState(val isLoading: Boolean = false)
+data class HistoryUiState(
+    val isLoading: Boolean = false,
+    val historyList: List<History> = emptyList(),
+    val error: String? = null,
+    val isEmpty: Boolean = false
+)
