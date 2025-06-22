b15c91cb811a1d32727d7130473fa994c79ad6c9
esmepantano <pantanoesmeralda@gmail.com>
2025-06-22 19:22:36 -0300

    modificación HistoriList

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/history/HistoryList.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/history/HistoryList.kt
@@ -1,33 +1,26 @@
 package ar.edu.unlam.scaffoldingandroid3.ui.history
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import ar.edu.unlam.scaffoldingandroid3.domain.model.History
 
 /**
- * TODO
- * Este historyList tendriamos que refactorizarlo para que reciba por parametro un
- * data class History que tenga toda la info para pasarle al HistoryCard y un onPlayClick Dinamico,
- * para que el composable no sea el que decida a donde va, si no, que sea la screen cuando lo
- * llame el que decida que info va y a donde redirige.
- * Para esta refactorizacion necesitamos crear un objeto de dominio, implementar la base de datos,
- * implementar un viewModel que se conecte con la screen, llamar con un mapper a los objetos de
- * la base de datos y pasarlos a dominio para que despues el view model se los pueda pasar a la screen
+ * Lista de historial de actividades
+ * Recibe por parámetro un data class History que tenga toda la info para pasarle al HistoryCard
+ * y un onDeleteClick dinámico, para que el composable no sea el que decida a donde va,
+ * sino que sea la screen cuando lo llame el que decida que info va y a donde redirige.
  */
 @Composable
 fun HistoryList(
-    date: String,
-    location: String,
-    distance: String,
-    duration: String,
+    historyList: List<History>,
+    onDeleteItem: (Long) -> Unit
 ) {
     LazyColumn {
-        items(15) { index ->
+        items(historyList) { history ->
             HistoryCard(
-                date = date,
-                location = location + index,
-                distance = distance + index,
-                duration = duration + index,
-                onPlayClick = {},
+                history = history,
+                onDeleteClick = { onDeleteItem(history.id) }
             )
         }
     }
